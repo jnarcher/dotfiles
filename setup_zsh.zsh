1df41945540446050ecaa3194f43fd079b76f7a7
Jalen Archer <118948506+jnarcher@users.noreply.github.com>
2023-08-03 01:38:17 -0700

    Add homebrew zsh to list of shells, set shell to zsh, symlink sh to zsh

--- setup_zsh.zsh
+++ setup_zsh.zsh
@@ -4,8 +4,23 @@ echo "\n<<< Starting ZSH Setup >>>\n"
 
 # Installation unnecessary; it's in the Brewfile
 
-echo "Enter superuser (sudo) password to edit /etc/shells"
-echo '/opt/homebrew/bin/zsh' | sudo tee -a '/etc/shells' >/dev/null
+if grep -Fxq "/opt/homebrew/bin/zsh" "/etc/shells"; then
+  echo "/opt/homebrew/bin/zsh already added to /etc/shells"
+else
+  echo "Enter superuser (sudo) password to edit /etc/shells"
+  echo "/opt/homebrew/bin/zsh" | sudo tee -a "/etc/shells" >/dev/null
+fi
 
-echo "Enter user password to change login shell"
-chsh -s '/opt/homebrew/bin/zsh'
+if [ "$SHELL" = '/bin/zsh' ]; then
+  echo "$SHELL is already /bin/zsh" 
+else
+  echo "Enter user password to change login shell"
+  chsh -s '/opt/homebrew/bin/zsh'
+fi
+
+if sh --version | grep -q zsh; then
+  echo "/private/var/select/sh already linked to /bin/zsh"
+else
+  echo "Enter superuser (sudo) password to symlink sh to zsh"
+  sudo ln -sfv /bin/zsh /private/var/select/sh
+fi
