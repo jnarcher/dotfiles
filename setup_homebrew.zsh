77cac84d8824c997a874205689f6c1181fe947ae
Jalen Archer <118948506+jnarcher@users.noreply.github.com>
2023-08-02 19:39:48 -0700

    Set global --no-quarantine option for Brewfile

--- setup_homebrew.zsh
+++ setup_homebrew.zsh
@@ -4,4 +4,9 @@ echo "\n<<< Starting Homebrew Setup >>>\n"
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
+
+# TODO: Might be possible in future to use `brew bundle --no-quarantine` as an option.
+# It's currently exported in zshrc:
+# export HOMEBREW_CASK_OPTS="--no-quarantine"
+
 brew bundle --verbose
