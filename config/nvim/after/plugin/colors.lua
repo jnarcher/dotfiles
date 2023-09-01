a383eef010fab69a082d032d20a53773a471e2db
Jalen Archer <118948506+jnarcher@users.noreply.github.com>
2023-09-01 11:32:34 -0700

    set cursor line highlight to transparent and bold/italic the number

--- config/nvim/after/plugin/colors.lua
+++ config/nvim/after/plugin/colors.lua
@@ -1,5 +1,7 @@
 -- Set background to be transparent
 function ColorMyPencils()
     vim.api.nvim_set_hl(0, 'Normal', { bg =  'none' })
+    vim.api.nvim_set_hl(0, 'CursorLine', { bg = 'none' })
+    vim.api.nvim_set_hl(0, 'CursorLineNR', { bold = true, italic = true })
 end
 ColorMyPencils()
