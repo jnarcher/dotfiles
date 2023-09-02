097d72a435b7bf4e1848648d7e62e363eed9dd43
Jalen Archer <118948506+jnarcher@users.noreply.github.com>
2023-09-02 00:15:01 -0700

    removed q keybindw

--- config/nvim/lua/jnarcher/remap.lua
+++ config/nvim/lua/jnarcher/remap.lua
@@ -30,6 +30,7 @@ vim.keymap.set('v', '<leader>d', '"_d')
 
 -- Remove `Q` keybind
 vim.keymap.set('n', 'Q', '<nop>')
+vim.keymap.set('n', 'q', '<nop>')
 
 -- Format current buffer
 vim.keymap.set('n', '<leader>f', function() vim.lsp.buf.format() end)
