47a3dbc76e51a8d5bf6824f5ee1ff6c13e086f10
Jalen Archer <118948506+jnarcher@users.noreply.github.com>
2024-06-14 13:42:14 -0700

    remove unneeded comment highlight

--- config/nvim/after/plugin/colors.lua
+++ config/nvim/after/plugin/colors.lua
@@ -34,6 +34,5 @@ function Colorize(color)
     vim.api.nvim_set_hl(0, 'CursorLine', { bg = 'none' })
     vim.api.nvim_set_hl(0, 'ColorColumn', { bg = '#17181c' })
     vim.api.nvim_set_hl(0, 'CursorLineNR', { bold = true, italic = true })
-    vim.api.nvim_set_hl(0, 'Comment', { italic = true })
 end
 Colorize()
