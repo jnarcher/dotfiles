13803c8b4f6c8e3c0cf23446b61a7e99c3a594e7
Jalen Archer <118948506+jnarcher@users.noreply.github.com>
2024-08-01 08:36:57 -0700

    change default colorscheme

--- config/nvim/after/plugin/colors.lua
+++ config/nvim/after/plugin/colors.lua
@@ -29,7 +29,7 @@ require('kanagawa').setup({
 })
 
 function Colorize(color)
-    color = color or 'rose-pine'
+    color = color or 'catppuccin-mocha'
     vim.cmd.colorscheme(color)
     vim.api.nvim_set_hl(0, 'CursorLine', { bg = 'none' })
     vim.api.nvim_set_hl(0, 'ColorColumn', { bg = '#17181c' })
