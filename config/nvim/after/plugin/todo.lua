4ce017686419381c54e99338766a514777e419d5
Jalen Archer <118948506+jnarcher@users.noreply.github.com>
2025-02-14 12:11:42 -0800

    change todo comment highlighting to be more subtle

--- config/nvim/after/plugin/todo.lua
+++ config/nvim/after/plugin/todo.lua
@@ -1,3 +1,10 @@
 require('todo-comments').setup({
-    signs = false,
+    gui_style = {
+        fg = "BOLD",
+    },
+    highlight = {
+        keyword = "fg",
+        after = "",
+        pattern = [[(KEYWORDS)]],
+    },
 })
